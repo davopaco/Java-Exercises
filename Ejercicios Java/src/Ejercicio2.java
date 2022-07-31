c4e2a77960427579de82ffe2be2174b6c70ff33d
davopaco <107714070+davopaco@users.noreply.github.com>
2022-07-31 12:54:58 -0500

    Update Ejercicio2.java

--- Ejercicios Java/src/Ejercicio2.java
+++ Ejercicios Java/src/Ejercicio2.java
@@ -1,23 +1,51 @@
 import java.util.*;
 
 public class Ejercicio2 {
-    public static void main(String[] args){
-        int numMayor=0, cantidadM=0, cantidad=0, entNum=0;
-        List<Integer> numeros=new ArrayList<Integer>();
+    public static void main(String[] args) {
+
+        int numMayor = 0, cantidadM = 0, cantidad = 0, entNum = 0, i = 0;
+        List<Integer> numeros = new ArrayList<Integer>();
+        Scanner entrada = new Scanner(System.in);
+        boolean rep = false;
 
         System.out.println("===SEGUNDO EJERCICIO===");
-        Scanner entrada= new Scanner(System.in);
-        System.out.println("Digita la cantidad de numeros a ingresar: ");
-        cantidad=entrada.nextInt();
 
-        for(int i=0;i<cantidad;i++){
-            System.out.println("Ingresa el numero "+(i+1)+": ");
-            entNum=entrada.nextInt();
-            numeros.add(entNum);
+        while (!rep) {
+            try {
+                if (cantidad == 0) {
+                    System.out.println("Digita la cantidad de numeros a ingresar: ");
+                    cantidad = entrada.nextInt();
+                }
+
+                if (cantidad < 0) {
+                    throw new ArithmeticException();
+                }
+
+                for (; i < cantidad; i++) {
+                    System.out.println("Ingresa el numero " + (i + 1) + ": ");
+                    entNum = entrada.nextInt();
+                    numeros.add(entNum);
+                }
+                rep = true;
+                entrada.close();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("\nIngrese un número valido.\n");
+            } catch (ArithmeticException e) {
+                System.out.println("\nLa cantidad no puede ser negativa.\n");
+            }
+    
+            if (!(cantidad < 0)) {
+                entrada.next();
+            } else if (cantidad < 0) {
+                cantidad = 0;
+            }
         }
-        numMayor=Collections.max(numeros,null);
-        cantidadM=Collections.frequency(numeros,numMayor);
-        System.out.println("El mayor numero leido fue: "+numMayor+"\nLa cantidad de veces que fue ingresado fue: "+cantidadM);
+
+        numMayor = Collections.max(numeros, null);
+        cantidadM = Collections.frequency(numeros, numMayor);
+        System.out.println("El mayor numero leido fue: " + numMayor + "\nLa cantidad de veces que fue ingresado fue: "
+                + cantidadM);
     }
 
 }

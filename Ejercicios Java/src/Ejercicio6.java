4e4165cdc6ac36c1b0d23a874cdd50442dd89c83
davopaco <107714070+davopaco@users.noreply.github.com>
2022-07-31 18:36:49 -0500

    Update Ejercicio6.java
    Did some modifications on the collections the class was using. From lists to arrays and using streams for filtering odd and even numbers.

--- Ejercicios Java/src/Ejercicio6.java
+++ Ejercicios Java/src/Ejercicio6.java
@@ -1,51 +1,35 @@
 import java.util.*;
 
 public class Ejercicio6 {
-    public static List doLists(List<Integer> l){
-        while (l.size()<10){
-            Integer random=0;
-            Random random1=new Random();
-            random=random1.nextInt(1000)+200;
-            l.add(random);
+    public static int[] hacerArreglos(int[] l) {
+        int random = 0;
+        for (int i = 0; i < 10; i++) {
+            Random random1 = new Random();
+            random = random1.nextInt(1000) + 200;
+            l[i] = random;
         }
         return l;
     }
 
     public static void main(String[] args) {
-        List<Integer> A = new LinkedList<>();
-        List<Integer> B = new LinkedList<>();
-        List<Integer> C =new LinkedList<>();
-        List<Integer> D =new LinkedList<>();
+        int[] A = new int[10];
+        int[] B = new int[10];
+        int[] temp = new int[10];
 
-        doLists(A);
-        doLists(B);
+        hacerArreglos(A);
+        hacerArreglos(B);
 
         System.out.println("===SEXTO EJERCICIO===");
-        System.out.println("El vector A es:\n" + A.toString());
-        System.out.println("\nEl vector B es:\n" + B.toString());
+        System.out.println("El vector A es:\n" + Arrays.toString(A));
+        System.out.println("\nEl vector B es:\n" + Arrays.toString(B));
 
-        Iterator<Integer> it1 = A.iterator();
-        Iterator<Integer> it2 = B.iterator();
+        A = Arrays.stream(A).filter(x -> x % 2 != 0).toArray();
+        B = Arrays.stream(B).filter(x -> x % 2 == 0).toArray();
+        temp = A;
+        A = B;
+        B = temp;
 
-        while(it1.hasNext()){
-            Integer num1=it1.next();
-            if(num1%2!=0){
-                C.add(num1);
-            }
-        }
-        while(it2.hasNext()){
-            Integer num2=it2.next();
-            if(num2%2==0){
-                D.add(num2);
-            }
-        }
-        A.clear();
-        B.clear();
-        A.addAll(D);
-        B.addAll(C);
-
-        System.out.println("\nEl nuevo vector A es:\n" + A.toString());
-        System.out.println("\nEl nuevo vector B es:\n" + B.toString());
+        System.out.println("\nEl nuevo vector A es:\n" + Arrays.toString(A));
+        System.out.println("\nEl nuevo vector B es:\n" + Arrays.toString(B));
     }
 }
-
